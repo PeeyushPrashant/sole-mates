a7465ae4ee905585bd8c80ae6ae64e6571692250
PeeyushPrashant <68290209+PeeyushPrashant@users.noreply.github.com>
2022-06-09 11:35:47 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { NavBar } from "./NavBar/Navbar";
 export { Aside } from "./Aside/Aside";
-export { VideoCard } from "./VideoCard/VideoCard.jsx";
-export { WatchLaterModal } from "./WatchLaterModal/WatchLaterModal.jsx";
-export { PlayListModal } from "./PlayListModal/PlayListModal.jsx";
+export { VideoCard } from "./VideoCard/VideoCard";
+export { WatchLaterModal } from "./WatchLaterModal/WatchLaterModal";
+export { PlayListModal } from "./PlayListModal/PlayListModal";
